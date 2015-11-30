cec7501f51184b0db3ef463b23cd9e87e52992de
thomas-lehmann <thomas.lehmann@immobilienscout24.de>
2015-11-30 18:08:12 +0100

    PIO-129: added s3 bucket to cfn template

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ use_plugin("pypi:pybuilder_aws_plugin")
 
 
 name = "aws-monocyte-alarming-lambda"
-version = "0.3"
+version = "0.4"
 summary = "aws-monocyte-alarming-lambda - Check SQS messages from monocyte for all ultimiate source of accounts (usofa) and send SES Emails via AWS Lambda"
 description = """ 
     Check SQS messages from monocyte for all ultimiate source of accounts and send SES Emails via AWS Lambda.
@@ -29,7 +29,7 @@ def set_properties(project):
     project.set_property("verbose", True)
 
     project.depends_on("boto3")
-    project.set_property("bucket_name", "aws-monocyte-alarming-lambda")
+    project.set_property("bucket_name", "aws-monocyte-alarming-lambda-infra")
     project.set_property("lambda_file_access_control", "private")
 
 
