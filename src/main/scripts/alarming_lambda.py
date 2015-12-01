f1de00ceff4fa9df0a046493950964f0a6e4be6e
thomas-lehmann <thomas.lehmann@immobilienscout24.de>
2015-12-01 17:32:01 +0100

    PIO-129 hotfix handler call

--- src/main/scripts/alarming_lambda.py
+++ src/main/scripts/alarming_lambda.py
@@ -4,6 +4,6 @@ from monocyte_alarm import MonocyteAlarm
 def handler(event, context):
     monocyte_alarm = MonocyteAlarm(sqs_queue='monocyte',
                                    sender_email='produktion@immobilienscout24.de',
-                                   recipients='thomas.lehmann@immobilienscout24.de',
+                                   recipients=['thomas.lehmann@immobilienscout24.de'],
                                    dry_run=True)
     monocyte_alarm()
